367692fd8fad46934f825a8f753fb74107e44368
Bray Moll <bkm82@nau.edu>
2023-09-23 18:45:59 -0700

    create an location for test cases

---
+++ src/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode . ((projectile-project-test-cmd . "pytest"))))
---
+++ src/test_no_slip_filter.py
@@ -0,0 +1,21 @@
+import pytest
+# from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
+ # from no_slip_filter import NoSlipFilter
+
+
+# @pytest.fixture
+# def no_slip_filter_instance():
+#     return NoSlipFilter()
+
+def test_test():
+    test_int = 1
+    assert test_int == 1
+
+def test_fail():
+    test_int =1
+    assert test_int ==2
+    
+    # def test_initialization(no_slip_filter_instance):
+#     #assert that the instance is not none
+#     assert no_slip_filter_instance is not None
+
