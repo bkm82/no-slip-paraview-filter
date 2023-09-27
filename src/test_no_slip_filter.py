85962613732cd945cbc53d54ca7dc5e07ea70dc0
Bray Moll <bkm82@nau.edu>
2023-09-27 00:26:25 -0700

    Test: Create unit test for Create_Mask method

--- src/test_no_slip_filter.py
+++ src/test_no_slip_filter.py
@@ -1,7 +1,8 @@
 
 import pytest
-# from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
+import numpy as np
 from no_slip_filter import NoSlipFilter
+from no_slip_filter import Create_Mask
 
 
 @pytest.fixture
@@ -23,3 +24,21 @@ def test_fail():
 def test_initialization(no_slip_filter_instance):
 
     assert no_slip_filter_instance is not None
+
+
+class Mesh:
+    def __init__(self, points):
+        self.Points = points
+
+
+def test_create_mask():
+    Volume_Mesh = Mesh(np.array(
+        [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
+    ))
+    Wall_Mesh = Mesh(np.array([[1.0, 2.0, 3.0], [7.0, 8.0, 8.0]]))
+
+    mask = Create_Mask(Volume_Mesh, Wall_Mesh)
+
+    expected_mask = np.array([True, False, False])
+
+    assert np.array_equal(mask, expected_mask)
